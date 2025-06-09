cae554344a41dbe563fee131af7736bf07bda1e6
TruongVu049 <nguyenvu492003@gmail.com>
2025-06-09 17:44:22 +0700

    chore(posts): add key

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -14,7 +14,10 @@ function PostsIndexComponent() {
       {posts.map(
         (post) => {
           return (
-            <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+            <div
+              key={post.id}
+              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+            >
               <Link
                 to="/posts/$postId"
                 params={{
